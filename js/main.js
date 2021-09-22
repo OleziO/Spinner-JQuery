38b3106b6b1802cebd51d1ce6d7c84e814f1b497
Herasymchuk Oleh <olezio.olezio@gmail.com>
2021-09-22 09:06:26 +0300

    added-click

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 $(function () {
-    let i = 1;
+    let degree  = 1;
     let speed = 0.1;
-    let anim;
-    let start = true;
-    $(document).keypress(function(event) {
-        if(start) {
-            anim = setInterval(() => {
-                $('.spinner-wrapper').css('transform', `rotate(${i+=(speed+=0.1)}deg)`);
+    let spinAnimation;
+    let isStarted = true;
+
+    let spin = () => {
+        if(isStarted) {
+            spinAnimation = setInterval(() => {
+                $('.spinner-wrapper').css('transform', `rotate(${degree += (speed += 0.1)}deg)`);
             }, 10);
-            start = false;
+            isStarted = false;
         }
         else {
-            speed = 2;
-            clearInterval(anim);
-            start = true;
+            speed = 0.1;
+            clearInterval(spinAnimation);
+            isStarted = true;
         }
-    });
+    }
 
+    $(document).keypress(spin);
+    $(document).click(spin);
 })
